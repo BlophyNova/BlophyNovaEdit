1493ab9e74e72a06f2e8d29af78d57e62ec513b7
HuaWaterED <1758158706@qq.com>
2024-09-01 16:35:32 +0800

    新增：感谢名单控件，可视化非线性过渡

--- Assets/Scripts/Data/Enumerate/LabelWindowContentType.cs
+++ Assets/Scripts/Data/Enumerate/LabelWindowContentType.cs
@@ -2,23 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [Flags]
 public enum LabelWindowContentType
 {
-    BPMList=1,
-    ChartPreview=2,
-    NoteEdit=4,
-    EventEdit=8,
-    Toolbar=16,
-    Menubar=32,
-    PropertyEdit=64,
-    Kawaii=128,
-    ProgressBar=256,
-    ATimeLine=512,
-    DebugText=1024,
-    NotePropertyEdit=2048,
-    AssociateLabelWindow=4096,
-    BoxList=8192,
-    RuntimeInspector=16384,
-    RuntimeHierarchy=32768
+    BPMList = 1,
+    ChartPreview = 2,
+    NoteEdit = 4,
+    EventEdit = 8,
+    Toolbar = 16,
+    Menubar = 32,
+    PropertyEdit = 64,
+    Kawaii = 128,
+    ProgressBar = 256,
+    ATimeLine = 512,
+    DebugText = 1024,
+    NotePropertyEdit = 2048,
+    AssociateLabelWindow = 4096,
+    BoxList = 8192,
+    RuntimeInspector = 16384,
+    RuntimeHierarchy = 32768,
+    VisualEase = 65536,
+    SpecialThanks = 131072
 }
---
+++ Assets/Scripts/Form/VisualEase/VisualEase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Form.NotePropertyEdit;
+using Scenes.DontDestroyOnLoad;
+using UnityEngine;
+
+public class VisualEase : LabelWindowContent,IRefresh
+{
+    public LineRenderer lineRenderer;
+    public RectTransform lineRendererRectTransform;
+
+    void Start()
+    {
+        lineRenderer.positionCount = 100;
+    }
+
+    public void Refresh()
+    {
+        if (labelWindow.associateLabelWindow.currentLabelWindow.labelWindowContentType ==
+            LabelWindowContentType.NotePropertyEdit)
+        {
+            NotePropertyEdit notePropertyEdit = (NotePropertyEdit)labelWindow.associateLabelWindow.currentLabelWindow;
+            EaseData ease = GlobalData.Instance.easeData[notePropertyEdit.ease.value];
+            Vector3[] positions = new Vector3[100];
+            Vector3[] corners = new Vector3[4];
+            lineRendererRectTransform.GetLocalCorners(corners);
+            for (int i = 0; i < positions.Length; i++)
+            {
+                //positions[i].
+                Vector3 currentPosition = (corners[2] - corners[0]) * (i / (float)positions.Length)+corners[0];
+                currentPosition.y = ease.thisCurve.Evaluate(i / (float)positions.Length) * (corners[2].y - corners[0].y)+corners[0].y;
+                currentPosition.z = -1;
+                positions[i] = currentPosition;
+            }
+            lineRenderer.SetPositions(positions);
+        }
+    }
+}
