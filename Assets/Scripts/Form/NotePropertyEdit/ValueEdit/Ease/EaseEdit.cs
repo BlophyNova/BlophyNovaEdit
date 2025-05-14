0717cb6c2c21c8dfb41bdd95ec8680ee99e7a48c
HuaWaterED <1758158706@qq.com>
2025-05-14 15:36:19 +0800

    feat: 音符编辑控件缓动部分UI搭建完成

--- Assets/Scripts/Form/NotePropertyEdit/ValueEdit/Ease/EaseEdit.cs
+++ Assets/Scripts/Form/NotePropertyEdit/ValueEdit/Ease/EaseEdit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,8 +7,34 @@ namespace Form.NotePropertyEdit.ValueEdit.Ease
 {
     public class EaseEdit : MonoBehaviour
     {
+        public TMP_Dropdown easeStyle;
         public TMP_InputField easeIndex;
         public TMP_Dropdown easeIO;
-        public TMP_Dropdown easeStyle;
+        public TMP_Dropdown easeOption;
+        public TextMeshProUGUI customEaseNameText;
+        public TMP_InputField customEaseName;
+
+        private void Start()
+        {
+            easeStyle.onValueChanged.AddListener(value =>
+            {
+                if (value == 0)
+                {
+                    customEaseNameText.gameObject.SetActive(false);
+                    customEaseName.gameObject.SetActive(false);
+                    easeIndex.interactable = true;
+                    easeIO.gameObject.SetActive(true);
+                    easeOption.gameObject.SetActive(true);
+                }
+                else
+                {
+                    customEaseNameText.gameObject.SetActive(true);
+                    customEaseName.gameObject.SetActive(true);
+                    easeIndex.interactable = false;
+                    easeIO.gameObject.SetActive(false);
+                    easeOption.gameObject.SetActive(false);
+                }
+            });
+        }
     }
 }
