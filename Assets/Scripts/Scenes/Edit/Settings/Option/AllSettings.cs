7886a713de17f09e0fd8722db6324b781c043ed7
HuaWaterED <1758158706@qq.com>
2025-06-10 13:27:12 +0800

    style: 画饼设置选项

--- Assets/Scripts/Scenes/Edit/Settings/Option/AllSettings.cs
+++ Assets/Scripts/Scenes/Edit/Settings/Option/AllSettings.cs
@@ -3,5 +3,13 @@ using UnityEngine;
 
 namespace Scenes.Edit.Settings.Option
 {
+    /*
+     * 自动保存间隔（单位s）
+     * 垂直同步
+     * 音乐大小
+     * 音效大小
+     * 鼠标滚轮速度
+     * 更改谱面元数据
+     */
     public class AllSettings : SwitchLabel { }
 }
