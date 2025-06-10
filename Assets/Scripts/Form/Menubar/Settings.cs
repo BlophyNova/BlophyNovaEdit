370c83103b72c19ebeb07f4dfdd007f7d4b982a1
HuaWaterED <1758158706@qq.com>
2025-06-10 09:20:24 +0800

    feat: 设置相关

---
+++ Assets/Scripts/Form/Menubar/Settings.cs
@@ -0,0 +1,20 @@
+using Scenes.PublicScripts;
+using UnityEngine;
+
+namespace Form.Menubar
+{
+    public class Settings : PublicButton
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
