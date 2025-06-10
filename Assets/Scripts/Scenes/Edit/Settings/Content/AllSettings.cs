9c7b28950f15e3a0ec36817bcce3b77af904ebc6
HuaWaterED <1758158706@qq.com>
2025-06-10 17:12:35 +0800

    feat: 搭建设置UI

--- Assets/Scripts/Scenes/Edit/Settings/Content/AllSettings.cs
+++ Assets/Scripts/Scenes/Edit/Settings/Content/AllSettings.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 namespace Scenes.Edit.Settings.Content
 {
+    /*
+     * 自动保存间隔（单位s）
+     * 垂直同步
+     * 帧数设置
+     * 音乐大小
+     * 音效大小
+     * 鼠标滚轮速度
+     * 更改谱面元数据
+     */
     public class AllSettings : MonoBehaviour
     {
         // Start is called before the first frame update
