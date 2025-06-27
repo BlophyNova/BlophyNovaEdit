a339a331b3146ff58d425c7b33c108dfa5acf05d
HuaWaterED <1758158706@qq.com>
2025-06-27 21:41:45 +0800

    fix: 修复#52

--- Assets/Scripts/Scenes/Edit/Settings/Option/Return.cs
+++ Assets/Scripts/Scenes/Edit/Settings/Option/Return.cs
@@ -1,4 +1,5 @@
 using Scenes.PublicScripts;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scenes.Edit.Settings.Option
@@ -6,10 +7,18 @@ namespace Scenes.Edit.Settings.Option
     public class Return : PublicButton
     {
 
+        public List<GameObject> canvas;
         // Start is called before the first frame update
         void Start()
         {
-            thisButton.onClick.AddListener(()=>Settings.Instance.gameObject.SetActive(false));
+            thisButton.onClick.AddListener(() => 
+            {
+                foreach (var item in canvas) 
+                {
+                    item.SetActive(false);
+                }
+                Settings.Instance.gameObject.SetActive(false);
+            });
         }
 
         // Update is called once per frame
